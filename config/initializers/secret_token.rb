e9524435a407a323bb59645d5e5172ce899d0687
Bryan <bknouse@u.rochester.edu>
2013-10-25 10:33:35 -0400

    first salsa commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Salsalito::Application.config.secret_key_base = 'e9d72f4c1ec4ca0c5fac2f718f4145a218a678533ec2f9718b1b75a6ccb91179ea6983fa2adceb238b0687e9fb8ecbaf8c1525e64f76aaa24547912153c1d04a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Salsalito::Application.config.session_store :cookie_store, key: '_salsalito_session'
